51d41473ace932846787039f5c683f82fcfb5487
HENRY BERGIN <tackyattack@gmail.com>
2017-12-28 21:57:07 -0600

    error checking for temperature

--- weather/temperature.sh
+++ weather/temperature.sh
@@ -7,6 +7,12 @@ read ZIP
 WEATHER_URL="https://www.wunderground.com/weather/us/$ZIP"
 search_content=$(wget --user-agent=Chrome $WEATHER_URL -q -O -)
 # You have to use E for extended regex
+ERR_CHECK=$(echo $search_content | grep -oE ".*")
 TEMPERATURE=$(echo $search_content | grep -oE "=\"current\-temp\".*?wu\-value wu\-value\-to\".*?>..")
 TEMPERATURE=$(echo $TEMPERATURE | grep -oE "..$")
-echo "Hello! The temperature in your area is $TEMPERATURE degrees Fahrenheit."
+if [ "$TEMPERATURE" = "" ]
+then
+  echo "There was an error. Wrong zipcode?"
+else
+  echo "Hello! The temperature in your area is $TEMPERATURE degrees Fahrenheit."
+fi
