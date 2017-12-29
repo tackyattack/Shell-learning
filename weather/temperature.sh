29ead90e19a212e3d3172052196cee2bd059b7e9
HENRY BERGIN <tackyattack@gmail.com>
2017-12-28 21:47:36 -0600

    temperature script

---
+++ weather/temperature.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This script gets the temperature of a zipcode
+# Regex tester: https://www.regextester.com/96872
+# Cheat sheet: http://www.rexegg.com/regex-quickstart.html
+echo "Enter the zipcode of where you wish to know the temperature:"
+read ZIP
+WEATHER_URL="https://www.wunderground.com/weather/us/$ZIP"
+search_content=$(wget --user-agent=Chrome $WEATHER_URL -q -O -)
+# You have to use E for extended regex
+TEMPERATURE=$(echo $search_content | grep -oE "=\"current\-temp\".*?wu\-value wu\-value\-to\".*?>..")
+TEMPERATURE=$(echo $TEMPERATURE | grep -oE "..$")
+echo "Hello! The temperature in your area is $TEMPERATURE degrees Fahrenheit."
